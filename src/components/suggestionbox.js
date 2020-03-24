16fe0dc9438d7b7d3cce16635753b0c45dd7d509
DevWabad <56509834+DevWabad@users.noreply.github.com>
2020-03-24 19:10:54 +0800

    added .js files for Home & suggestion box

---
+++ src/components/suggestionbox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class suggestionbox extends Component {
+    render(){
+        return (
+            <div>
+                Suggestion box
+            </div>
+        );
+    }
+}
+
+export default suggestionbox;
